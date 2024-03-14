1f6b3f27a3be0976f8d9265118e8a79a78646e9e
antonin-tarrade <antotarrade@gmail.com>
2024-03-14 13:43:43 +0100

    Axe median fonctionnel

--- Donnees/extraction_squelette.m
+++ Donnees/extraction_squelette.m
@@ -9,35 +9,82 @@ function squelette = extraction_squelette(img_binaire)
 %    l'image initiale
 
     P = get_initial_point(img_binaire);
-
-    [nb_row, nb_col] = size(img_binaire);
     
-    contour = bwtraceboundary(img_binaire,P,"NW");
+    contour = flip(bwtraceboundary(img_binaire,P,"NW"),2);
     
     if(~isempty(contour))
         figure;
         imshow(img_binaire);
         hold on;
-        plot(contour(:,2), contour(:,1), 'g', 'LineWidth', 2);
+        plot(contour(:,1), contour(:,2), 'g', 'LineWidth', 2);
     end
 
+   
     dt = delaunayTriangulation(contour(:,2),contour(:,1));
     
     [V, R] = voronoiDiagram(dt);
 
-%     V_sorted = zeros(size(V,1),1);
-% 
-%      for i=1:(size(V,1)) 
-%          v1 = floor(floor(V(i,1)));
-%          v2 = floor(V(i,2));
-%         if v1 > nb_row || v2 > nb_col || v1 <= 0 || v2 <= 0
-%             V_sorted(i) = 0;
-%         else
-%             V_sorted(i) = img_binaire(v1,v2)~= 0;
-%         end
-%     end 
+     V_final = sort_edges(img_binaire,V);
+     scatter(V_final(:,1), V_final(:,2), 'm', 'filled');
 
-    scatter(V(:, 1), V(:, 2), 'm', '+');
+    for i = 1:size(R,1)
+        indices = R{i};  
+        vertices = flip(V(indices,:),2);
+
+        vertices_sorted = sort_vertex(img_binaire,vertices);
+        plot(vertices_sorted(:,1), vertices_sorted(:,2), 'b', 'LineWidth', 1);
+
+
+%         valid_vertices = all(vertices >= 1 & vertices <= [nb_col, nb_row], 2);
+%         valid_region_vertices = vertices(valid_vertices, :);
+% 
+%         
+%         if size(valid_region_vertices, 1) > 1
+%             plot(valid_region_vertices(:,1), valid_region_vertices(:,2), 'b', 'LineWidth', 1);
+%         end
+    end
+
     hold off;
+
 end
 
+function position_sorted = sort_edges(img_binaire,positions)
+    n = size(positions,1);
+    [nb_row,nb_col] = size(img_binaire);
+    mask = repmat(zeros(n,1),1,2);
+
+    for i=1:n
+        x = floor(positions(i,1));
+        y = floor(positions(i,2));
+
+        if x > nb_row || y > nb_col || x <= 0 || y <= 0
+            mask(i,:) = 0;
+        else
+            mask(i,:) = img_binaire(x,y)~= 0;
+        end
+    end
+
+    position_sorted = flip(reshape(positions(logical(mask)),[],2),2);
+end
+
+
+
+function position_sorted = sort_vertex(img_binaire,positions)
+    positions = flip(positions,2);
+    n = size(positions,1);
+    [nb_row,nb_col] = size(img_binaire);
+    mask = repmat(zeros(n,1),1,2);
+
+    for i=1:n
+        x = floor(positions(i,1));
+        y = floor(positions(i,2));
+
+        if x > nb_row || y > nb_col || x <= 0 || y <= 0
+            mask(i,:) = 0;
+        else
+            mask(i,:) = img_binaire(x,y)~= 0;
+        end
+    end
+
+    position_sorted = flip(reshape(positions(logical(mask)),[],2),2);
+end
