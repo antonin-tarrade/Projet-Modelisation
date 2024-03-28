1ddd50f49b50f0ac5f4bc0d222c9c8d36d64432b
Romain-Malinge <romain.malinge.pro@gmail.com>
2024-03-28 20:04:49 +0100

    p3 faces

--- Donnees/PROJET_P3.m
+++ Donnees/PROJET_P3.m
@@ -3,34 +3,32 @@ close all;
 clc;
 load("donnee_pour_P3.mat")
 
+
 % Calcul des faces uniques à partir des tétraèdres
-Faces = [tri(:, [1, 2, 3]); tri(:, [1, 2, 4]); tri(:, [1, 3, 4]); tri(:, [2, 3, 4])];
+faces = [tri(:, [1, 2, 3]); tri(:, [1, 2, 4]); tri(:, [1, 3, 4]); tri(:, [2, 3, 4])];
 
+% tri des faces par ordre croissant pour les 3 sommets 
+faces = sortrows(sort(faces,2));
 
-% Trier les faces par ordre croissant de ses sommets
-Sorted_Faces = sortrows(Faces, [1 2 3]);
-
-
-% Supression des faces en doubles : 
-i = 1;
-while i < size(Sorted_Faces,1)
-    if Sorted_Faces(i,:) == Sorted_Faces(i+1,:)
-        Sorted_Faces(i+1,:) = [];
-    else
-        i = i+1;
+to_del = [];
+for face = 1:size(faces,1)-1
+    if sum(faces(face,:) == faces(face+1,:)) == 3
+        to_del = [to_del face face+1];
     end
-    
 end
 
+% Supprimer les surfaces marquées pour suppression
+faces(unique(to_del),:) = [];
 
-fprintf('Calcul du maillage final terminé : %d faces. \n',size(Sorted_Faces,1));
+
+fprintf('Calcul du maillage final terminé : %d faces. \n',size(faces,1));
 
 % Affichage du maillage final
 figure;
 hold on
-for i = 1:size(Sorted_Faces,1)
-   plot3([X(1,Sorted_Faces(i,1)) X(1,Sorted_Faces(i,2))],[X(2,Sorted_Faces(i,1)) X(2,Sorted_Faces(i,2))],[X(3,Sorted_Faces(i,1)) X(3,Sorted_Faces(i,2))],'r');
-   plot3([X(1,Sorted_Faces(i,1)) X(1,Sorted_Faces(i,3))],[X(2,Sorted_Faces(i,1)) X(2,Sorted_Faces(i,3))],[X(3,Sorted_Faces(i,1)) X(3,Sorted_Faces(i,3))],'r');
-   plot3([X(1,Sorted_Faces(i,3)) X(1,Sorted_Faces(i,2))],[X(2,Sorted_Faces(i,3)) X(2,Sorted_Faces(i,2))],[X(3,Sorted_Faces(i,3)) X(3,Sorted_Faces(i,2))],'r');
+for i = 1:size(faces,1)
+   plot3([X(1,faces(i,1)) X(1,faces(i,2))],[X(2,faces(i,1)) X(2,faces(i,2))],[X(3,faces(i,1)) X(3,faces(i,2))],'r');
+   plot3([X(1,faces(i,1)) X(1,faces(i,3))],[X(2,faces(i,1)) X(2,faces(i,3))],[X(3,faces(i,1)) X(3,faces(i,3))],'r');
+   plot3([X(1,faces(i,3)) X(1,faces(i,2))],[X(2,faces(i,3)) X(2,faces(i,2))],[X(3,faces(i,3)) X(3,faces(i,2))],'r');
 end
 
